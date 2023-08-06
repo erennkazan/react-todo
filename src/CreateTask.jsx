ba7f848fc1e6728f3e99830fd97472498a847f8b
A00183340\AR100003 <erennkazan@gmail.com>
2023-08-06 13:22:36 +0300

    task usestate created

--- src/CreateTask.jsx
+++ src/CreateTask.jsx
@@ -1,16 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function CreateTask() {
+
+const [task,setTask] = useState({
+    title:'',
+    content:''
+})
+function detectChange(event){
+    console.log(event.target.value)
+}
   return (
     <div className='todoDivArea'>
       <form className='todoDiv'>
-        <input type="text"
+        <input 
+        type="text"
         className='form-control todoText mb-3'
         name='title'
+        onChange={detectChange}
+        value={task.title}
         placeholder='Başlık'
         />
-        <textarea name="" 
+        <textarea 
+        name="content" 
+        value={task.content}
         className='form-control todoText mb-3'
+        onChange={detectChange}
         rows={3}
         placeholder='İşinizi Yazınız'
         ></textarea>
